706eb530b085e9f1082fc34d9624e96db8c08f08
Rashmi Amarasinghe <rhamarasinghe@gmail.com>
2026-05-22 15:02:06 +0530

    Configured AppDbContext and SQL Server connection with initial Entity Framework migration

---
+++ TaskManagement.API/Models/TaskItem.cs
@@ -0,0 +1,22 @@
+﻿namespace TaskManagement.API.Models;
+
+public class TaskItem
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    public int Priority { get; set; }
+
+    public DateTime? DueDate { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public int UserId { get; set; }
+
+    public User User { get; set; }
+}
---
+++ TaskManagement.API/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagement.API.Models;
+
+public class User
+{
+    public int Id { get; set; }
+
+    public string Username { get; set; }
+
+    public string Password { get; set; }
+}
